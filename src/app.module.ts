91ebe1c0b0d752f40c8ec3df0f914133f93fa878
xiukun <xiukun@163.com>
2026-03-31 17:32:27 +0800

    feat: 添加 @nestjs/serve-static 模块及配置

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,17 @@ import { AiModule } from './ai/ai.module';
 import { ToolModule } from './tool/tool.module';
 import { ConfigModule } from '@nestjs/config';
 import { SpeechModule } from './speech/speech.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [AiModule, ConfigModule.forRoot({
     isGlobal: true, envFilePath: '.env'
-  }), SpeechModule
+  }),
+  ServeStaticModule.forRoot({
+    rootPath: join(__dirname, '..', 'client'),
+  })
+  , SpeechModule
   ],
   controllers: [AppController],
   providers: [AppService],
